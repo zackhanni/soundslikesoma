94aea0b47203f73b3289f42364f0100b5d283758
Zack Hanni <zack.hannI@gmail.com>
2023-02-15 15:24:25 -0500

    fix google analytics connection

--- assets/js/defer-scripts.js
+++ assets/js/defer-scripts.js
@@ -1,12 +1,6 @@
 
 // content from google-analitics html
-window.dataLayer = window.dataLayer || [];
 
-function gtag() {
-dataLayer.push(arguments);
-}
-gtag('js', new Date());
-gtag('config', '{{ site.data.seo.google_analytics_id }}');
 
 
 // other defer'd JavaScript...
